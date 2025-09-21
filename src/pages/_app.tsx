53a06f498b0b4f6bcc2a2547d3fe456896151ff8
Apriloracle <86314121+Apriloracle@users.noreply.github.com>
2025-09-21 14:01:58 -0400

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,12 +4,15 @@ import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { Web3Provider } from '@/components/Web3Provider'
 import { SubdocumentProvider } from '@/contexts/SubdocumentContext'
+import AppRouter from '@/components/AppRouter';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Web3Provider>
       <SubdocumentProvider>
-        <Component {...pageProps} />
+        <AppRouter>
+          <Component {...pageProps} />
+        </AppRouter>
       </SubdocumentProvider>
     </Web3Provider>
   )
