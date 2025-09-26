e6a0acd2058e3d68b0e5d0cc3e2b36d1541d316c
Apriloracle <86314121+Apriloracle@users.noreply.github.com>
2025-09-26 17:47:00 -0400

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import { Metadata } from 'next'; // Add this import
+
+export async function generateMetadata(): Promise<Metadata> {
+    return {
+        other: {
+        'fc:miniapp': JSON.stringify({
+            version: 'next',
+            imageUrl: 'https://reap.deals/embed-image.png',
+            button: {
+                title: `launch Reap`,
+                action: {
+                    type: 'launch_miniapp',
+                    name: 'Reap',
+                    url: 'https://reap.deals',
+                    splashImageUrl: 'https://reap.deals/200x200.png',
+                    splashBackgroundColor: '#ffffff',
+                },
+            },
+        }),
+        },
+    };
+}
