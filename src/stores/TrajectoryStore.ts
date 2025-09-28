14374baddd61202cf935ffbaa28d2a68238551ff
Apriloracle <86314121+Apriloracle@users.noreply.github.com>
2025-09-27 22:10:01 -0400

    Add files via upload

---
+++ src/stores/TrajectoryStore.ts
@@ -0,0 +1,20 @@
+import { createStore } from 'tinybase';
+import { createLocalPersister } from 'tinybase/persisters/persister-browser';
+
+const store = createStore().setSchema({
+  trajectory: {
+    timestamp: { type: 'number', default: 0 },
+    x: { type: 'number', default: 0 },
+    y: { type: 'number', default: 0 },
+    scale: { type: 'number', default: 0 },
+    query: { type: 'string', default: '' },
+    rawOutput: { type: 'number', default: 0 },
+  },
+});
+
+export const persister = createLocalPersister(store, 'trajectory');
+
+persister.startAutoLoad();
+persister.startAutoSave();
+
+export default store;
