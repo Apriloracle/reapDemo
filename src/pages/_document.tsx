c6fb9766c484972680f1cc2fdff2a35b141864a1
Apriloracle <86314121+Apriloracle@users.noreply.github.com>
2025-10-04 17:19:34 -0400

    Update _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,7 +7,7 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
-          <script src="https://unpkg.com/ml5@0.12.2/dist/ml5.min.js" defer />
+          <script src="" defer />
           <link rel="manifest" href="/manifest.json" />
         </Head>
         <body>
