a0cf11a73b1774c6731c6c26959beb83f399edd2
Apriloracle <86314121+Apriloracle@users.noreply.github.com>
2025-10-17 17:10:45 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import ProductsPage from './pages/ProductsPage';
-import HypervectorSearchTest from './components/HypervectorSearchTest';
 
 const App: React.FC = () => {
   return (
     <Routes>
       <Route path="/products" element={<ProductsPage />} />
-      <Route path="/hypervector-test" element={<HypervectorSearchTest />} />
     </Routes>
   );
 };
