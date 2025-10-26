a89dcbf39964d90f75cbdea7779ed8cb530add55
Apriloracle <86314121+Apriloracle@users.noreply.github.com>
2025-10-25 21:33:45 -0400

    Update ProductDetailPage.tsx

--- src/pages/ProductDetailPage.tsx
+++ src/pages/ProductDetailPage.tsx
@@ -150,7 +150,7 @@ const ProductDetailPage: React.FC = () => {
       </div>
 
       <div className={styles.detailsContainer}>
-        <p className={styles.price}>{product.price}</p>
+        <p className={styles.price}>{product.priceDisplay}</p>
         <h1 className={styles.title}>{product.name}</h1>
         <p className={styles.source}>{product.source}</p>
 
@@ -187,7 +187,6 @@ const ProductDetailPage: React.FC = () => {
 };
 
 export default ProductDetailPage;
-
 
 
 
