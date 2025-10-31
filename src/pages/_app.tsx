63368d6ff98ff39a18ef2953e3b82b3c3ce7a12e
Apriloracle <86314121+Apriloracle@users.noreply.github.com>
2025-10-30 23:39:58 -0400

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,20 +1,48 @@
 'use client';
 
 import '@/styles/globals.css'
+import '@/styles/Wallet.css'
 import type { AppProps } from 'next/app'
 import { Web3Provider } from '@/components/Web3Provider'
 import { SubdocumentProvider } from '@/contexts/SubdocumentContext'
 import AppRouter from '@/components/AppRouter';
+import React, { useMemo } from 'react';
+import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
+import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
+import { PhantomWalletAdapter, SolflareWalletAdapter } from '@solana/wallet-adapter-wallets';
+import {
+    WalletModalProvider
+} from '@solana/wallet-adapter-react-ui';
+import { clusterApiUrl } from '@solana/web3.js';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  // Default styles that can be overridden by your app
+  require('@solana/wallet-adapter-react-ui/styles.css');
+  
+  const network = WalletAdapterNetwork.Devnet;
+  const endpoint = useMemo(() => clusterApiUrl(network), [network]);
+  const wallets = useMemo(
+      () => [
+          new PhantomWalletAdapter(),
+          new SolflareWalletAdapter(),
+      ],
+      [network]
+  );
+
   return (
-    <Web3Provider>
-      <SubdocumentProvider>
-        <AppRouter>
-          <Component {...pageProps} />
-        </AppRouter>
-      </SubdocumentProvider>
-    </Web3Provider>
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={wallets} autoConnect>
+        <WalletModalProvider>
+          <Web3Provider>
+            <SubdocumentProvider>
+              <AppRouter>
+                <Component {...pageProps} />
+              </AppRouter>
+            </SubdocumentProvider>
+          </Web3Provider>
+        </WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
   )
 }
 
