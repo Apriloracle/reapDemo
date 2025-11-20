007681c22d7e35e7f190bdfddb6d5d8616871a11
Apriloracle <86314121+Apriloracle@users.noreply.github.com>
2025-11-19 22:37:52 -0500

    Implement useCrayonShoppingStore hook

---
+++ src/hooks/useCrayonShoppingStore.ts
@@ -0,0 +1,39 @@
+import { useStore, useTable } from 'tinybase/ui-react';
+import { favoriteStore } from '../stores/FavoriteStore';
+import { searchIndexStore } from '../stores/SearchIndexStore';
+import { userProfileStore } from '../stores/UserProfileStore';
+
+export const useCrayonShoppingStore = () => {
+  const cart = useTable('cart', favoriteStore.getStore());
+  const wishlist = useTable('wishlist', favoriteStore.getStore());
+  const searchHistory = useTable('searches', searchIndexStore);
+  const walletAddress = useStore(userProfileStore.getStore())?.getValue('walletAddress');
+
+  const addToCart = (item: any) => {
+    favoriteStore.addFavorite({ ...item, isWishlistItem: false });
+  };
+
+  const addToWishlist = (item: any) => {
+    favoriteStore.addFavorite({ ...item, isWishlistItem: true });
+  };
+
+  const addSearchHistory = (query: string) => {
+    const id = new Date().toISOString();
+    searchIndexStore.setRow('searches', id, { query, timestamp: id });
+  };
+
+  const connectWallet = (address: string) => {
+    userProfileStore.updateProfile({ walletAddress: address });
+  };
+
+  return {
+    cart,
+    wishlist,
+    searchHistory,
+    walletAddress,
+    addToCart,
+    addToWishlist,
+    addSearchHistory,
+    connectWallet,
+  };
+};
