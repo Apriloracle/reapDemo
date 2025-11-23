75e99eaf176841f4a914d8c2fca9b713f9edaef6
Apriloracle <86314121+Apriloracle@users.noreply.github.com>
2025-11-23 16:42:58 -0500

    Update Agent.tsx

--- src/pages/Agent.tsx
+++ src/pages/Agent.tsx
@@ -1,4 +1,8 @@
-import CrayonAgent from '../components/CrayonAgent';
+import dynamic from "next/dynamic";
+
+const CrayonAgent = dynamic(() => import("../components/CrayonAgent"), {
+  ssr: false
+});
 
 export default function AgentPage() {
   return <CrayonAgent />;
