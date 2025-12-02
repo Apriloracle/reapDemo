8ccbb8c5394b4b195953d25122902ac4adf0190a
Apriloracle <86314121+Apriloracle@users.noreply.github.com>
2025-12-02 15:58:48 -0500

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,28 +9,46 @@ import type { AppProps } from 'next/app'
 import { Web3Provider } from '@/components/Web3Provider'
 import { SubdocumentProvider } from '@/contexts/SubdocumentContext'
 import AppRouter from '@/components/AppRouter';
-import React from 'react';
-import dynamic from 'next/dynamic';
-
-const WalletContextProvider = dynamic(
-  () => import('../components/WalletContextProvider').then((mod) => mod.WalletContextProvider),
-  { ssr: false }
-);
+import React, { useMemo } from 'react';
+import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
+import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
+import { PhantomWalletAdapter, SolflareWalletAdapter } from '@solana/wallet-adapter-wallets';
+import {
+    WalletModalProvider
+} from '@solana/wallet-adapter-react-ui';
+import { clusterApiUrl } from '@solana/web3.js';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  // Default styles that can be overridden by your app
+  require('@solana/wallet-adapter-react-ui/styles.css');
+  
+  const network = WalletAdapterNetwork.Mainnet;
+  const endpoint = process.env.NEXT_PUBLIC_HELIUS_RPC_URL!;
+
+  const wallets = useMemo(
+      () => [
+          new PhantomWalletAdapter(),
+          new SolflareWalletAdapter(),
+      ],
+      [network]
+  );
+
   return (
-    <WalletContextProvider>
-      <Web3Provider>
-        <SubdocumentProvider>
-          <AppRouter>
-            <Component {...pageProps} />
-          </AppRouter>
-        </SubdocumentProvider>
-      </Web3Provider>
-    </WalletContextProvider>
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={wallets} autoConnect>
+        <WalletModalProvider>
+          <Web3Provider>
+            <SubdocumentProvider>
+              <AppRouter>
+                <Component {...pageProps} />
+              </AppRouter>
+            </SubdocumentProvider>
+          </Web3Provider>
+        </WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
   )
 }
 
 export default MyApp
-
 
