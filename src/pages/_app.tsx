050f713feb7fb8732e74790e0fe4c214cdfebac3
Apriloracle <86314121+Apriloracle@users.noreply.github.com>
2025-12-02 16:06:34 -0500

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,7 +9,7 @@ import type { AppProps } from 'next/app'
 import { Web3Provider } from '@/components/Web3Provider'
 import { SubdocumentProvider } from '@/contexts/SubdocumentContext'
 import AppRouter from '@/components/AppRouter';
-import React, { useMemo } from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
 import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
 import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
 import { PhantomWalletAdapter, SolflareWalletAdapter } from '@solana/wallet-adapter-wallets';
@@ -19,6 +19,12 @@ import {
 import { clusterApiUrl } from '@solana/web3.js';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
   // Default styles that can be overridden by your app
   require('@solana/wallet-adapter-react-ui/styles.css');
   
@@ -34,19 +40,31 @@ function MyApp({ Component, pageProps }: AppProps) {
   );
 
   return (
-    <ConnectionProvider endpoint={endpoint}>
-      <WalletProvider wallets={wallets} autoConnect>
-        <WalletModalProvider>
-          <Web3Provider>
-            <SubdocumentProvider>
-              <AppRouter>
-                <Component {...pageProps} />
-              </AppRouter>
-            </SubdocumentProvider>
-          </Web3Provider>
-        </WalletModalProvider>
-      </WalletProvider>
-    </ConnectionProvider>
+    <>
+      {isClient ? (
+        <ConnectionProvider endpoint={endpoint}>
+          <WalletProvider wallets={wallets} autoConnect>
+            <WalletModalProvider>
+              <Web3Provider>
+                <SubdocumentProvider>
+                  <AppRouter>
+                    <Component {...pageProps} />
+                  </AppRouter>
+                </SubdocumentProvider>
+              </Web3Provider>
+            </WalletModalProvider>
+          </WalletProvider>
+        </ConnectionProvider>
+      ) : (
+        <Web3Provider>
+          <SubdocumentProvider>
+            <AppRouter>
+              <Component {...pageProps} />
+            </AppRouter>
+          </SubdocumentProvider>
+        </Web3Provider>
+      )}
+    </>
   )
 }
 
