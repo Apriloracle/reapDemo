f419e014a858e7c32033d4959a75426a34a06901
Apriloracle <86314121+Apriloracle@users.noreply.github.com>
2025-12-06 19:38:58 -0500

    Create hypercore-test.tsx

---
+++ src/pages/hypercore-test.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import HypercoreLogger from '../components/HypercoreLogger';
+
+const HypercoreTestPage: React.FC = () => {
+  return (
+    <div>
+      <h1>Hypercore Test Page</h1>
+      <HypercoreLogger />
+    </div>
+  );
+};
+
+export default HypercoreTestPage;
