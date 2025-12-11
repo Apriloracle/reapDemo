e39c9ea8d4fca6f28a6f5794ea8e63ee1011701a
Apriloracle <86314121+Apriloracle@users.noreply.github.com>
2025-12-10 23:09:49 -0500

    Update AgentFetcher.tsx

--- src/components/AgentFetcher.tsx
+++ src/components/AgentFetcher.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useTable } from 'tinybase/ui-react';
 import { agentDataStore } from '../stores/AgentDataStore';
+import AgentFetcher from '../components/AgentFetcher';
 import { useSearchParams } from 'react-router-dom';
 
 const AgentExplorerPage = () => {
