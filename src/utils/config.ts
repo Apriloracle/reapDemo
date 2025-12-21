b86568ca8ce26d39bd49038cadf831ae4db60f16
Apriloracle <86314121+Apriloracle@users.noreply.github.com>
2025-12-20 23:10:01 -0500

    Add base and polygon chains to config

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,12 +1,14 @@
 import { http, createConfig } from 'wagmi'
-import { mainnet, sepolia, celo, celoAlfajores } from 'wagmi/chains'
+import { mainnet, sepolia, celo, celoAlfajores, base, polygon } from 'wagmi/chains'
 
 export const config = createConfig({
-  chains: [mainnet, sepolia, celo, celoAlfajores],
+  chains: [mainnet, sepolia, celo, celoAlfajores, base, polygon,],
   transports: {
     [mainnet.id]: http(),
     [sepolia.id]: http(),
     [celo.id]: http(),
     [celoAlfajores.id]: http(),
+    [base.id]: http(),
+    [polygon.id]: http(),
   },
 })
